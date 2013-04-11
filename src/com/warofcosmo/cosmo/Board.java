8443c330e8a2e9b70f059e5ae0f8447c0aced799
michaelbocock <michaelbocock@10.102.1.115>
2013-04-11 07:05:51 -0600

    Level Entity changes

--- src/com/warofcosmo/cosmo/Board.java
+++ src/com/warofcosmo/cosmo/Board.java
@@ -3,7 +3,6 @@ package com.warofcosmo.cosmo;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
